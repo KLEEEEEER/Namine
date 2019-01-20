78fee5c0f8b0d8a73708f97e4a3ea48151b553af
Maxim <gonzalx_747@mail.ru>
2019-01-20 20:52:59 +0900

    Added modification file template

--- namineJs.js
+++ namineJs.js
@@ -1,43 +1,61 @@
 var fs = require('fs');
 var path = require('path');
-"use strict"
+"use strict";
 var extension_name = 'NAME';
 var filt = new RegExp("//-nmn "+extension_name+"((\\s*).*(\\s*))//-nmn", 'gmu');
+var write_filename = './modifications/'+extension_name+'.ocmod.xml';
 
+function writeModificationFileStart() {
+	// console.log('writeModificationFileStart');
+	var start_string = `<?xml version="1.0" ?>
+<!DOCTYPE modification [
+	]>
+<modification>
+	<name>`+extension_name+`</name>
+	<version>0.1</version>
+	<author></author>
+	<link></link>
+	<code></code>`;
+	fs.writeFileSync(write_filename, start_string, function(err){
+		if (err) throw err;
+	});
+}
 
-
-// var walkSync = function(dir, filelist) {
-// 	var fs = fs || require('fs'),
-// 		files = fs.readdirSync(dir);
-// 	filelist = filelist || [];
-// 	files.forEach(function(file) {
-// 		if (fs.statSync(dir + '/' + file).isDirectory()) {
-// 			filelist = walkSync(dir + '/' + file, filelist);
-// 		}
-// 		else {
-// 			filelist.push(file);
-// 		}
-// 	});
-// 	return filelist;
-// };
-// var file_list = walkSync('./');
-// console.log(file_list);
+function writeModificationFileEnd() {
+	// console.log('writeModificationFileEnd');
+	var end_string = `
+</modification>`;
+	fs.appendFileSync(write_filename, end_string, function(err){
+		if (err) throw err;
+	});
+}
 
 function writeModificationFile(filename, modifications_array) {
+	// console.log('writeModificationFile');
 	var data = '';
-	console.log(modifications_array);
-	// for (var i=0; i<modifications_array.length; i++) {
-	// 	data += modifications_array[i][1];
-	// }
-	modifications_array.forEach(function(item, i, arr){
-		console.log('WHAT');
-		data += i + '\n';
-		data += item[0] + item[1] + '\n';
-	});
 
-	fs.writeFile('./modifications/modification1.txt', data, function(err){
+	for (var key in modifications_array) {
+		if (modifications_array.hasOwnProperty(key)) {
+			if (modifications_array[key][0] !== undefined && modifications_array[key][1] !== undefined) {
+				data += `	<file path="`+key+`">
+		<operation>
+			<search>
+				<![CDATA[`;
+				data += `]]></search>
+			<add position="before"><![CDATA[`;
+				// data += 'Character: ' + modifications_array[key][0] + ' ' + modifications_array[key][1] + '\n';
+				data += modifications_array[key][1];
+				data += `]]></add>
+		</operation>
+	</file>`;
+				data+='\n';
+			}
+		}
+	}
+
+	// fs.writeFile(write_filename, data, function(err){
+	fs.appendFileSync(write_filename, data, function(err){
 		if (err) throw err;
-		console.log('The file has been saved!');
 	});
 }
 
@@ -52,7 +70,7 @@ function fromDir(startPath, filter, filelist){
 		var filename=path.join(startPath,files[i]);
 		var stat = fs.lstatSync(filename);
 		if (stat.isDirectory()){
-			fromDir(filename,filter,filelist); //recurse
+			fromDir(filename,filter,filelist);
 		}
 		else if (filename.indexOf(filter)>=0) {
 			filelist.push(filename);
@@ -63,18 +81,19 @@ function fromDir(startPath, filter, filelist){
 
 var file_list = fromDir('./','.php');
 
+writeModificationFileStart();
 for (var i=0; i<file_list.length; i++) {
-
 	(function(filename){
-		fs.readFile(filename, 'utf8', function(err, contents) {
-				var modifications = [];
-				modifications[filename] = [];
-				var match;
-				while ((match = filt.exec(contents)) !== null) {
-					modifications[filename].push(match['index'], match[1]);
-					console.log(match[1]);
-					writeModificationFile(filename, modifications);
-				}
-		});
+		var contents = fs.readFileSync(filename);
+		var modifications;
+		var match;
+		while ((match = filt.exec(contents)) !== null) {
+			modifications = [];
+			modifications[filename] = [];
+			modifications[filename].push(match['index'], match[1]);
+			writeModificationFile(filename, modifications);
+		}
+
 	})(file_list[i]);
 }
+writeModificationFileEnd();
