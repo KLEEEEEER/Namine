7484d9e2c8d60d418678d989568ccdfce32918ab
Maxim <gonzalx_747@mail.ru>
2019-01-20 22:04:35 +0900

    Added parameters and more extensions to work

--- namineJs.js
+++ namineJs.js
@@ -1,8 +1,34 @@
+var extension_name = 'test';
+var author = 'Author';
+var link = '';
+var version = '0.0001';
+
+process.argv.forEach(function (val, index, array) {
+	var value_array = val.split('=');
+	switch (value_array[0]) {
+		case '-name':
+			extension_name = value_array[1];
+			break;
+		case '-author':
+			author = value_array[1];
+			break;
+		case '-link':
+			link = value_array[1];
+			break;
+		case '-ver':
+			version = value_array[1];
+			break;
+		default:
+			break;
+	}
+});
+
 var fs = require('fs');
 var path = require('path');
 "use strict";
-var extension_name = 'NAME';
-var filt = new RegExp("//-nmn "+extension_name+"((\\s*).*(\\s*))//-nmn", 'gmu');
+// var filt = new RegExp("//-nmn "+extension_name+"((\\s*).*(\\s*))//-nmn", 'gmu');
+var filt = new RegExp('//-nmn '+extension_name+' pos:"(.*)" line:"(.*)"((\\s*).*(\\s*))//-nmn', 'gmu');
+var filt_html = new RegExp('<!--//-nmn '+extension_name+' pos:"(.*)" line:"(.*)"-->((\\s*).*(\\s*))<!--//-nmn-->', 'gmu');
 var write_filename = './modifications/'+extension_name+'.ocmod.xml';
 
 function writeModificationFileStart() {
@@ -12,10 +38,10 @@ function writeModificationFileStart() {
 	]>
 <modification>
 	<name>`+extension_name+`</name>
-	<version>0.1</version>
-	<author></author>
-	<link></link>
-	<code></code>`;
+	<version>`+version+`</version>
+	<author>`+author+`</author>
+	<link>`+link+`</link>
+	<code>`+extension_name+`</code>`;
 	fs.writeFileSync(write_filename, start_string, function(err){
 		if (err) throw err;
 	});
@@ -36,13 +62,19 @@ function writeModificationFile(filename, modifications_array) {
 
 	for (var key in modifications_array) {
 		if (modifications_array.hasOwnProperty(key)) {
-			if (modifications_array[key][0] !== undefined && modifications_array[key][1] !== undefined) {
-				data += `	<file path="`+key+`">
+			if ( modifications_array[key][0] !== undefined
+				&& modifications_array[key][1] !== undefined
+				&& modifications_array[key][2] !== undefined
+				&& modifications_array[key][3] !== undefined
+			) {
+				data += `	
+	<file path="`+key+`">
 		<operation>
 			<search>
 				<![CDATA[`;
+				data += modifications_array[key][3];
 				data += `]]></search>
-			<add position="before"><![CDATA[`;
+			<add position="`+modifications_array[key][2]+`"><![CDATA[`;
 				// data += 'Character: ' + modifications_array[key][0] + ' ' + modifications_array[key][1] + '\n';
 				data += modifications_array[key][1];
 				data += `]]></add>
@@ -79,21 +111,31 @@ function fromDir(startPath, filter, filelist){
 	return filelist;
 }
 
-var file_list = fromDir('./','.php');
+var file_list_php = fromDir('./catalog/','.php');
+var file_list_tpl = fromDir('./catalog/','.tpl');
+var file_list_twig = fromDir('./catalog/','.twig');
+
+function find_match(list, filter) {
+	for (var i=0; i<list.length; i++) {
+		(function(filename){
+			// console.log('readFileSync ' + filename);
+			var contents = fs.readFileSync(filename);
+			var modifications;
+			var match;
+			while ((match = filter.exec(contents)) !== null) {
+				// console.log(match);
+				modifications = [];
+				modifications[filename] = [];
+				modifications[filename].push(match['index'], match[3], match[1], match[2]);
+				writeModificationFile(filename, modifications);
+			}
+
+		})(list[i]);
+	}
+}
 
 writeModificationFileStart();
-for (var i=0; i<file_list.length; i++) {
-	(function(filename){
-		var contents = fs.readFileSync(filename);
-		var modifications;
-		var match;
-		while ((match = filt.exec(contents)) !== null) {
-			modifications = [];
-			modifications[filename] = [];
-			modifications[filename].push(match['index'], match[1]);
-			writeModificationFile(filename, modifications);
-		}
-
-	})(file_list[i]);
-}
+find_match(file_list_php, filt);
+find_match(file_list_tpl, filt_html);
+find_match(file_list_twig, filt_html);
 writeModificationFileEnd();
